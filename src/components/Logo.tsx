30827bf75f48dba959f8c8851a4ace3241863b55
Gregory Davis <gdavis@gregm4.local>
2025-09-06 11:20:17 -0400

    feat(splash): fixed many issues

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,18 +8,10 @@ interface LogoProps {
 }
 
 export const Logo: React.FC<LogoProps> = ({ size = 48, style }) => {
-  const { themeMode } = useTheme();
-
-  // Use different logo based on theme
-  const logoSource =
-    themeMode === "dark"
-      ? require("../../assets/images/logo/logo-dark.svg")
-      : require("../../assets/images/logo/logo-light.svg");
-
   return (
     <View style={[{ width: size, height: size, backgroundColor: 'transparent' }, style]}>
       <Image
-        source={logoSource}
+        source={require("../../assets/splash.png")}
         style={{
           width: size,
           height: size,
